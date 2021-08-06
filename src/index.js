e3311fc1f8f9bdf846ec515f6c07a766ec7eb2b0
Artem Drozd <artsdrd@gmail.com>
2021-08-06 23:13:23 +0300

    fix: fix solution

--- src/index.js
+++ src/index.js
@@ -1,48 +1,12 @@
 module.exports = function toReadable(number) {
 
-    const belowTwenty = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 
-    'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const belowTwenty = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',
+        'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
     const belowHundred = ['zero', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
 
     let i = 0;
     let stage = arguments[0];
     let result = '';
-
-    if (number === 100) {
-        return 'one hundred';
-    }
-
-    if (number === 200) {
-        return 'two hundred';
-    }
-
-    if (number === 300) {
-        return 'three hundred';
-    }
-
-    if (number === 400) {
-        return 'four hundred';
-    }
-
-    if (number === 500) {
-        return 'five hundred';
-    }
-
-    if (number === 600) {
-        return 'six hundred';
-    }
-
-    if (number === 700) {
-        return 'seven hundred';
-    }
-
-    if (number === 800) {
-        return 'eight hundred';
-    }
-
-    if (number === 900) {
-        return 'nine hundred';
-    }
 
     if (number < 20) {
         result = belowTwenty[number];
@@ -51,16 +15,20 @@ module.exports = function toReadable(number) {
         i = number % 10;
         result = belowHundred[Math.floor(number / 10)];
 
-        if (i != 0){
+        if (i != 0) {
             result += ' ' + belowTwenty[i];
         }
 
     } else if (number < 1000) {
-        result = toReadable(Math.floor(number / 100)) + ' hundred ' + toReadable(number % 100);
-        
+        i = number % 100;
+        if (i != 0) {
+            result = toReadable(Math.floor(number / 100)) + ' hundred ' + toReadable(number % 100);
+        } else
+            result = toReadable(Math.floor(number / 100)) + ' hundred';
     };
+
     return result;
-}
-    
+};
+
 
 
